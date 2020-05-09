084c78cd042b0065e297caa1aa15caf20d645779
HHHHH-Y <984495875@qq.com>
2020-05-09 23:05:00 +0800

    git all

--- 课件习题/汉诺塔问题/src/Hanoi.java
+++ 课件习题/汉诺塔问题/src/Hanoi.java
@@ -1,16 +1,16 @@
 public class Hanoi {
     // 汉诺塔问题
-    public static void hanoi(int n, char A, char B, char C) {
+    public static void hanoi(int n, char pose1, char pose2, char pose3) {
         if(n == 1) {
-            move(A, C);
+            move(pose1, pose3);
         } else {
-            hanoi(n-1, A, C, B);
-            move(A, C);
-            hanoi(n-1, B, A, C);
+            hanoi(n-1, pose1, pose3, pose2);
+            move(pose1, pose3);
+            hanoi(n-1, pose2, pose1, pose3);
         }
     }
-    public static void move(char A, char C) {
-        System.out.println(A + "-->" + C);
+    public static void move(char pose1, char pose2) {
+        System.out.println(pose1 + "-->" + pose2);
     }
     public static void main(String[] args) {
         hanoi(3, 'A', 'B', 'C');
